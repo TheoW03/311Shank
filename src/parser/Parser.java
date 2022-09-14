a99dca40138b2a12c8f5a4059f074ec3e2eca34d
Theo <95878514+TheoW03@users.noreply.github.com>
2022-09-14 17:12:34 -0400

    So close.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -21,25 +21,15 @@ import java.util.*;
  */
 public class Parser {
     private ArrayList<Token> tokens;
-    private Token tok, next;
+    private Token tok, next; //remove next
     private Parser rootNode;
     public Node left, right;
     public Node element;
     private Node root;
     private Node curr;
-    private ArrayList<Node> lisOfNodes;
 
     public Parser(ArrayList<Token> tokens) {
         this.tokens = tokens;
-        lisOfNodes = new ArrayList<>();
-    }
-
-    public Parser(Node element) {
-        this.element = element;
-    }
-
-    public Parser(Parser ro) {
-        this.rootNode = ro;
     }
 
     /**
@@ -63,10 +53,10 @@ public class Parser {
     }
 
     /**
-     *
      * @return next node.
      */
 
+    //remove
     public Node Next() {
         this.next = tokens.get(0);
         tokens.remove(0);
@@ -79,39 +69,72 @@ public class Parser {
 
     /**
      * yep. Its not Solving. it works but it isnt solving.
+     * remove.
      */
     public Node parse() {
-        if (tokens.size() == 2) {
-            // the problem is here. for some reason if I dont terminate higher than 2 it throws a IndexOutOFboundsException
-            //on me. (if your willing to patch that will be nice. but I tried it and IDK how to get rid of it).
-            return root;
-        }
+//        if (tokens.size() == 2) {
+//            // the problem is here. for some reason if
+//            I dont terminate higher than 2 it throws a IndexOutOFboundsException
+//            //
+//            on me. (if your willing to patch that will be nice. but I tried it and IDK how to get rid of it).
+//            return root;
+        //enforcing order
+        //3+2*5
+        //
+        // EX
+//        }
         Node curr = parserMethodThis();
         Node nextS = Next();
-        if(curr == null || nextS == null){
-            parse();
-        }
+        System.out.println(nextS + " " + curr);
+//        if (curr == null || nextS == null) {
+//            return root;
+//        }
+        //so match and remove is a search method??
+        //how it works
+        /**
+         * find number,
+         * 5+3*5/2
+         * 5 is our 1st token
+         * ex calls term
+         *
+         * term calls factor. calls match and remove on times (Term calls so calls until it finds PEMDAS)
+         * Factor can do match and remove
+         * check if anything is left.
+         * then factor finds a multiplication.
+         * return null if and only if u find nothing.
+         *
+         */
         this.root = expression();
+        if(nextS == null){
+            return root;
+        }
         parse();
         return root;
     }
 
     /**
-     *
      * @return new node
      * EXPRESSION = TERM { (plus or minus) TERM} :')
      */
+    //match and remove
+    //call term
     public Node expression() {
-        Node node = term();
+        Node node = term(); //returns a mathOPNode.
+
+        if(next == null){
+            return nomeral(tok);
+        }
         return new MathOpNode(node, term(), next);
     }
 
     /**
      * FACTOR = {-} number or lparen EXPRESSION rparen
+     *
      * @return node
      */
+    //use match and remove */
     public Node Factor() {
-        if (nomeral(tok) != null) {
+        if (nomeral(tok) != null) { //and for some reason () has a null prob. Idek. :')
             return nomeral(tok);
         } else if (tok.getTokenAsString().equals("(()")) {
             return expression();
@@ -122,6 +145,7 @@ public class Parser {
 
     /**
      * TERM = FACTOR { (times or divide) FACTOR}
+     *
      * @return
      */
     public Node term() {
@@ -143,8 +167,7 @@ public class Parser {
             //number.
             try {
                 int a = Integer.parseInt(value.getTokenName());
-                IntegerNode a2 = new IntegerNode(a);
-                return a2;
+                return new IntegerNode(a);
             } catch (NumberFormatException e2) {
                 return null;
             }
@@ -163,15 +186,16 @@ public class Parser {
         }
         return new MathOpNode(token.getTokenAsString());
     }
+
     /**
      * @param token
      */
+    //compares head of the list. and returns null.
+
     public String matchAndRemove(String token) {
         if (token.equals(tokens.get(0).getTokenAsString())) {
-
             return tokens.remove(0).getTokenName();
         }
-
         return null;
     }
 
