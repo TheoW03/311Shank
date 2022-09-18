118b1774a61ad4ad1a4f2be12d639adc06aa86e0
Theo <95878514+TheoW03@users.noreply.github.com>
2022-09-17 23:01:18 -0400

    HELP

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -43,16 +43,21 @@ public class Lexer {
         String buffer;
         String OPperLine = "";
         int state = 1;
+        char currentChar;
 
         for (int i1 = 0; i1 < data.size(); i1++) { //loop
             String dataTokensLine = data.get(i1); //token
-            int i = 0; //i
             buffer = "";
-            while (i < dataTokensLine.length()) {
-                char currentChar = dataTokensLine.charAt(i); //each token
+            for (int i = 0; i < dataTokensLine.length(); ++i) {
+                System.out.println("top");
+                currentChar = dataTokensLine.charAt(i); //each token
                 //ooperator.
                 if (currentChar != ' ') {
                     if (state == 1) {
+                        if (!buffer.equals("")) {
+                            tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
+                            buffer = "";
+                        }
                         switch (currentChar) {
                             case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
                                 state = 3;
@@ -66,62 +71,42 @@ public class Lexer {
                                 tokenDataR.add(new Token(Token.OPTokens.ADD, "+"));
                             }
                             case '-' -> {
-                                if (!buffer.equals("")) {
-                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                    buffer = "";
-                                }
+
                                 state = 2;
                                 tokenDataR.add(new Token(Token.OPTokens.SUBTRACT, "-"));
                             }
                             case '*' -> {
-                                if (!buffer.equals("")) {
-                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                    buffer = "";
-                                }
                                 tokenDataR.add(new Token(Token.OPTokens.MULTIPLY, "*"));
                             }
                             case '/' -> {
-                                if (!buffer.equals("")) {
-                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                    buffer = "";
-                                }
                                 tokenDataR.add(new Token(Token.OPTokens.DIVIDE, "/"));
                             }
                             case ' ' -> tokenDataR.add(new Token(Token.OPTokens.ENDOFLINE, "/"));
+                            default -> state = -1;
                         }
                         //Idk
                     } else if (state == 2) {
 
                     } else if (state == 3) { //Es nomeral.
+                        System.out.println("buffer (num): " + buffer + " index: " + i);
                         switch (currentChar) {
-                            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+                            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> { // is number. this loops
                                 buffer += String.valueOf(currentChar);
                                 System.out.println("cond1 state: " + state);
+                                System.out.println("buffer en state 3: " + buffer);
                             }
                             case '+', '-', '*', '/', ';' -> {
+                                if (!buffer.equals("")) {
+                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
+                                    buffer = "";
+                                }
                                 if (currentChar == '+') {
-                                    if (!buffer.equals("")) {
-                                        tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                        buffer = "";
-                                    }
                                     tokenDataR.add(new Token(Token.OPTokens.ADD, "+"));
                                 } else if (currentChar == '-') {
-                                    if (!buffer.equals("")) {
-                                        tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                        buffer = "";
-                                    }
                                     tokenDataR.add(new Token(Token.OPTokens.SUBTRACT, "-"));
                                 } else if (currentChar == '*') {
-                                    if (!buffer.equals("")) {
-                                        tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                        buffer = "";
-                                    }
                                     tokenDataR.add(new Token(Token.OPTokens.MULTIPLY, "*"));
                                 } else {
-                                    if (!buffer.equals("")) {
-                                        tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-                                        buffer = "";
-                                    }
                                     tokenDataR.add(new Token(Token.OPTokens.DIVIDE, "/"));
                                 }
                                 state = 1;
@@ -129,6 +114,9 @@ public class Lexer {
                                 System.out.println("char?: " + currentChar);
                             }
                             case '.' -> {
+                                if (state == 5 || buffer.contains(".")) {
+                                    throw new UnauthTokenException("Error There 2 decimals");
+                                }
                                 state = 5;
                                 System.out.println("cond3 state: " + state);
                             }
@@ -137,218 +125,22 @@ public class Lexer {
                     } else if (state == 4) {
 
                     } else if (state == 5) { //es floatar.
-                        System.out.println("es decimal");
-
                         buffer += ".";
-                        System.out.println(buffer);
                         state = 3;
+                        System.out.println("buffer (Dec): " + buffer + " index: " + i);
+                        System.out.println("state: " + state);
+
                     } else if (state == 6) {
                     } else {
                         throw new UnauthTokenException("error");
                     }
+                } else {
+                    System.out.println("space");
                 }
-//                switch (token){
-//                    case '0':
-//
-//                }
-
-//                if (number(token) != 11) {
-//                    if (!buffer.equals("")) {
-//                        tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
-//                        buffer = "";
-//                    }
-//                    state = 1;
-//                } else if (number(token) == -1) {
-//                    state = 5;
-//                } else {
-//                    state = 3;
-//
-//                }else{
-//
-//                }
-                System.out.println(i + " current char: " + currentChar);
-//                switch (state) {
-//                    case 1: //es Operator
-//                        System.out.println("state: "+state);
-//                        switch (currentChar) {
-//                            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-//                                state = 3;
-//                                buffer += currentChar;
-//                                System.out.println("buffer: "+buffer);
-//                                break;
-//                            case '+':
-//                                if(!buffer.equals("")){
-//                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER,buffer));
-//                                    buffer = "";
-//                                }
-//                                System.out.println("plus: state1: ite"+i);
-//                                state = 2;
-//                                tokenDataR.add(new Token(Token.OPTokens.ADD, "+"));
-//                                break;
-//                            case '-':
-//                                if(!buffer.equals("")){
-//                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER,buffer));
-//                                    buffer = "";
-//                                }
-//                                state = 2;
-//                                tokenDataR.add(new Token(Token.OPTokens.SUBTRACT, "-"));
-//                                break;
-//                            case '*':
-//                                if(!buffer.equals("")){
-//                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER,buffer));
-//                                    buffer = "";
-//                                }
-//                                tokenDataR.add(new Token(Token.OPTokens.MULTIPLY, "*"));
-//                                break;
-//                            case '/':
-//                                if(!buffer.equals("")){
-//                                    tokenDataR.add(new Token(Token.OPTokens.NUMBER,buffer));
-//                                    buffer = "";
-//                                }
-//                                tokenDataR.add(new Token(Token.OPTokens.DIVIDE, "/"));
-//                                break;
-//                            case' ':
-//                                tokenDataR.add(new Token(Token.OPTokens.ENDOFLINE, "/"));
-//                                break;
-//                        }
-//                        break;
-//                    case 2: //es negative or addition.
-//                        System.out.println("state: "+state);
-//                        switch (currentChar){
-//                            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-//                                state = 3;
-//                                System.out.println("cond1 state: "+state);
-////                                buffer +="-" + currentChar;
-//                            case '+':
-//                                tokenDataR.add(new Token(Token.OPTokens.ADD,"+"));
-//                                System.out.println("cond2 state: "+state);
-//                                break;
-//                            case '-':
-//                                tokenDataR.add(new Token(Token.OPTokens.SUBTRACT,"-"));
-//                                System.out.println("cond3 state: "+state);
-//                                break;
-//                        }
-//                        break;
-//                    case 3: //nomeral.
-//                        System.out.println("state: "+state);
-//                        System.out.println("buffer: "+buffer);
-//                        switch (currentChar) {
-//                            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-//                                buffer += String.valueOf(currentChar);
-//                                System.out.println("cond1 state: "+state);
-//                                break;
-//                            case '+', '-','*','/',';':
-//                                state = 1;
-//                                System.out.println("cond2 state: "+state);
-//                                System.out.println("char?: "+currentChar);
-//                                break;
-//                            case '.':
-//                                state = 5;
-//                                System.out.println("cond3 state: "+state);
-//                                break;
-//                        }
-//                    case 5:
-//                        buffer += String.valueOf(currentChar);
-//                        state = 2;
-//                        break;
-//                    //decimal
-//                    default:
-//                        System.out.println("state: error"+state);
-//                        throw new UnauthTokenException("line " + lineNum + " at char " + i +
-//                                " token '" + currentChar +
-//                                " is unauthorized");
-//
-//                }
-//
-//                if(token != ' '){ //ignore space
-//                    //es Mathmatica state
-//                    if(tokenList.compare(token) != null) {
-//                        //subract
-//                        if(token == '-'){
-//                            if(checkIfNOpISRpeated >= 2){ //if 2---3
-//                                errorState=true;
-//                            }
-//                            //somehow works. I made this really cheeky :')
-//                            if(!numbre.equals("")){ //break numbre
-//                                tokenDataR.add(new Token(Token.OPTokens.NUMBER, numbre));
-//                                numbre = "";
-//                            }
-//                            if(checkIfNOpISRpeated == 0){ //if Its 2-3
-//                                checkIfNOpISRpeated++;
-//                                tokenDataR.add(new Token(token,tokenList.compare(token)));
-//                                i++;
-//                                continue;
-//                            }
-//
-//                            numbre += String.valueOf(token); //adds
-//                            checkIfNOpISRpeated++;
-//
-//                        }else{ //if +*/
-//                            if(token == '('){
-//                                tokenDataR.add(new Token(token,tokenList.compare(token)));
-//                                i++;
-//                                continue;
-//                            }
-//
-//                            if(checkIfOPRepeated >= 2 && token != ')'){ //checks for 2***3 or 2++4
-//                                errorState=true;
-//                            }
-//                            checkIfOPRepeated++;
-//                            checkIfNOpISRpeated++;
-//                            if(!numbre.equals("")){ //check if being used in eq
-//                                tokenDataR.add(new Token(Token.OPTokens.NUMBER, numbre));
-//                                numbre = "";
-//                            }
-//                            if(OPperLine.equals("") &&  token != ')'){  //checks for 2+*3
-//                                OPperLine = tokenList.compare(token);
-//                            }
-//                            if(token == ')'){
-//                                tokenDataR.add(new Token(token,tokenList.compare(token)));
-//                                i++;
-//                                continue;
-//                            }
-//                            if(OPperLine.equals(tokenList.compare(token) ) ){ //adds to list
-//                                tokenDataR.add(new Token(token,tokenList.compare(token)));
-//                            }else{
-//                                errorState = true;
-//                            }
-//
-//                        }
-//
-//                    }else if(tokenList.esNumbre(token)){ //123
-//                        checkIfNOpISRpeated = 0;
-//                        checkIfOPRepeated = 0;
-//                        OPperLine = "";
-//                        numbre += String.valueOf(token);
-//                    }else if(token == '.'){ //decimal
-//                        checkIfNOpISRpeated = 0;
-//                        checkIfOPRepeated = 0;
-//                        //play with later
-//                        if(esDeciamal){ ///1.2.34.
-//                            errorState = true;
-//                        }
-//                        if(dataTokensLine.charAt(i-1) != token && i >= 1 && tokenList.esNumbre(dataTokensLine.charAt(i-1))){
-//                            numbre += String.valueOf(token);
-//                            esDeciamal = true;
-//                        }else{
-//                            errorState=true;
-//                        }
-//
-//                    }else{
-//                        errorState = true;
-//                    }
-//
-//                }else{
-//                    i++;
-//                    continue;
-//                }
-//                if(errorState){ //error throws exception
-//                    throw new UnauthTokenException("line "+lineNum+" at char "+i+" token '"+token+"' is unauthorized");
-//                }
-                i++;
-            }
+                System.out.println("next char");
+            } //eof
             if (!buffer.equals("")) {
-                System.out.println(buffer);
+                System.out.println("end of loop");
                 tokenDataR.add(new Token(Token.OPTokens.NUMBER, buffer));
                 buffer = "";
             }
